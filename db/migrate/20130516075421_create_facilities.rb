01ea2a0db7f97d0ab5eb137dd33a25914b22af1b
yudi <yudi@yudi-pc.(none)>
2013-05-21 16:23:21 +0700

    remove unnecessary files , update cart_item

--- db/migrate/20130516075421_create_facilities.rb
+++
@@ -1,9 +0,0 @@
-class CreateFacilities < ActiveRecord::Migration
-  def change
-    create_table :facilities do |t|
-    	t.string :name
-    	t.integer :booking_id
-      t.timestamps
-    end
-  end
-end
