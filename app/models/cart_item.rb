80acd27f2c28464f849eb1d44ea47d2c447b3aaa
Mahesa <googs@googs.(none)>
2013-05-22 17:58:51 +0700

    jreng

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -11,16 +11,26 @@ class CartItem < ActiveRecord::Base
 
   	if start_time.present?
       self.start_time = Time.parse("#{self.check_in_date} #{self.start_time}")
-  	  errors.add("Start Time", "should be greater than now") if self.start_time < Time.now
+      errors.add("Start Time", "should be greater than now") if self.start_time < Time.now
     end
   	
     if end_time.present?
       self.end_time = Time.parse("#{self.check_in_date} #{self.end_time}")	  
-  	  errors.add("End Time", "should be greater than now") if self.end_time < Time.now
+      errors.add("End Time", "should be greater than now") if self.end_time < Time.now
   	end
   
   	if start_time && end_time && start_time > end_time
-  		errors.add("End Time", "should be greater than Start Time")
+      errors.add("End Time", "should be greater than Start Time")
   	end
+
+    #self.class --> CartItem.where
+
+    if OrderItem.where(room_id: self.room_id).
+                 where(check_in_date: check_in_date).
+                 where("(start_time <= ? AND end_time > ?) OR (start_time < ? AND end_time >= ?) OR (start_time > ? AND end_time < ?)", 
+                        self.start_time, self.start_time, self.end_time, self.end_time, self.start_time, self.end_time).
+                 present?
+      self.errors.add("room", "is already used by another company")
+    end
   end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,13 +2,19 @@ class OrderItem < ActiveRecord::Base
   belongs_to :room
   belongs_to :order
   attr_accessible :order_id , :start_time, :end_time, :check_in_date, :price, :room_id
-  # validate :check_avilability
+  validate :check_availability
 
-  # private 
-  #   def check_availability
-  #   	if where("start_time >= ? AND start_time < ?", self.start_time, self.end_time).present?
-  #   		self.errors.add(:start_time, "is already used by another company")
-  #   	end
-  #   end
+  private 
+    def check_availability
+    	if OrderItem.where(room_id: self.room_id).
+                 where(check_in_date: check_in_date).
+                 where("(start_time <= ? AND end_time > ?) OR (start_time < ? AND end_time >= ?) OR (start_time > ? AND end_time < ?)", 
+                        self.start_time, self.start_time, self.end_time, self.end_time, self.start_time, self.end_time).present?
+								# menggunakan sql query untuk nested conditions
+								#
+
+        self.errors.add("room", "is already used by another company")
+      end
+    end
 
 end
