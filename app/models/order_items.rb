a029f131d41ee01ee3c4694908b7319b4ae6618d
Mahesa <googs@googs.(none)>
2013-05-22 17:59:12 +0700

    tes

--- app/models/order_items.rb
+++
@@ -1,6 +0,0 @@
-class OrderItem < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :order
-  belongs_to :room
-  attr_accessible :start_time, :end_time, :price, :room_id
-end
--- app/models/reservation.rb
+++
@@ -1,3 +0,0 @@
-class Reservation < ActiveRecord::Base
-  # attr_accessible :title, :body
-end
