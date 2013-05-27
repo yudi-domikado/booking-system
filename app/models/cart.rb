0e02a66b2a75beaa3f9be3e4a9e6e0318710406b
Yudi <adul@domikado.com>
2013-05-27 09:08:29 +0700

    sedot

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,14 +5,14 @@ class Cart < ActiveRecord::Base
 
 
   def self.add_item(session_cart,params)
-	  cart = find_or_create_by_session_id(session_cart)
-	  cart_item = cart.cart_items.find_or_initialize_by_room_id(params[:room_id])
-	  cart_item.check_in_date = params[:check_in_date]
-	  cart_item.start_time = params[:start_time]
-	  cart_item.end_time = params[:end_time]
-	  cart_item.price = cart_item.room.price
-	  cart_item.save
-	  cart_item
-	end
+	cart = find_or_create_by_session_id(session_cart)
+	cart_item = cart.cart_items.find_or_initialize_by_room_id(params[:room_id])
+	cart_item.check_in_date = params[:check_in_date]
+	cart_item.start_time = params[:start_time]
+	cart_item.end_time = params[:end_time]
+	cart_item.price = cart_item.room.price
+	cart_item.save
+	cart_item
+  end
 	
 end
